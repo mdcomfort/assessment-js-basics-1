35366da680d2a241c59e9d1060ce45a399954615
Maya Comfort <mdcomfort@gmail.com>
2021-10-17 20:10:26 -0400

    made going further changes

--- password.js
+++ password.js
@@ -11,9 +11,19 @@ console.log("Welcome to the password validator tool!")
 
 reader.question("What is your password?", function(input) {
     user_password = input
-    if (user_password.length >= 10) {
+    if (user_password.endsWith(".")) {
+        console.log("Sorry, your password cannot end with a period.") 
+    } 
+    else if (user_password.includes("@")) {
+        console.log("Sorry, your password cannot contain an @ symbol.")
+    } 
+    else if (user_password.length >= 10) {
         console.log("Password created.")
-    } else {
+        console.log(`+-+-+-+ +-+-+-+-+
+|A|l|l| |s|e|t|!|
++-+-+-+ +-+-+-+-+`)
+    } 
+    else if (user_password.length < 10) {
         console.log("Sorry, that password isn't long enough. Password must be at least 10 characters long.")
     }
 
